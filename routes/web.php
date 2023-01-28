06b64c6c16efc5d85d21451958457137144fc1e7
Turgut <turgutsalgin5534@gmail.com>
2023-01-29 01:49:35 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,28 @@ Route::get('/', [SiteController::class, 'index'])->name('site.index');
 Route::get('lang/change', [SiteController::class, 'change'])->name('changeLang');
 
 Route::get('/login', [SiteController::class, 'index'])->name('site.index');
+
+Route::get('/config-clear', function() {
+    Artisan::call('config:clear');
+    // Do whatever you want either a print a message or exit
+});
+
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    // Do whatever you want either a print a message or exit
+});
+
+Route::get('/cache-clear', function() {
+    Artisan::call('cache:clear');
+    // Do whatever you want either print a message or exit
+});
+
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    // Do whatever you want either print a message or exit
+});
+
+Route::get('/storage-link', function() {
+    Artisan::call('storage:link');
+    // Do whatever you want either print a message or exit
+});
